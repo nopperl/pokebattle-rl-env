e316a2fdb129d63700aab41c6501dd5cdf079dfc
nikuya3 <nikuya@dummy.mail>
2018-02-08 13:13:20 +0100

    Began implementing class structure

--- pokemon_battle_rl_env/battle_env.py
+++ pokemon_battle_rl_env/battle_env.py
@@ -1,4 +1,5 @@
 from gym import Env
+from gym.spaces import Box, MultiBinary
 
 from pokemon_battle_rl_env.showdown_simulator import ShowdownSimulator
 
@@ -7,10 +8,14 @@ class BattleEnv(Env):
     def __init__(self, simulator=ShowdownSimulator()):
         self.__version__ = "0.1.0"
         self.simulator = simulator
+        self.action_space = MultiBinary(10)  # Attack using one of 4 moves, switch to one of 5 pokemon or pass
+        self.observation_space
+        self.reward_range = (-1, 1)
+        # ToDo: Set metadata['render.modes']
 
     def _step(self, action):
         self.simulator.attack(1)
-        reward = 1 if self.simulator.state.state == 'won' else 0
+        reward = 1 if self.simulator.state.state == 'won' else -1 if self.simulator.state.state == 'lost' else 0
         return self.simulator.state.to_array(), reward, self.simulator.state.state != 'ongoing', None
 
     def _reset(self):
--- pokemon_battle_rl_env/game_state.py
+++ pokemon_battle_rl_env/game_state.py
@@ -1,6 +1,47 @@
 import numpy as np
 
 
+class Item:
+    def __init__(self, name):
+        self.name = name
+        self.used = False
+
+
+class Move:
+    def __init__(self, name, pp, target='normal', disabled=False):
+        self.name = name
+        self.pp = pp
+        # add self.type = [type query]
+        self.target = target
+        self.disabled = disabled
+
+
+class Stats:
+    def __init__(self, atk, def_, spa, spd, spe):
+        self.attack = atk
+        self.def_ = def_
+        self.spa = spa
+        self.spd = spd
+        self.spe = spe
+
+
+class Pokemon:
+    def __init__(self, name, gender, stats, moves, ability, item, health=1.0):
+        self.name = name
+        self.health = health
+        self.gender = gender
+        self.stats = stats
+        self.moves = moves
+        self.ability = ability
+        self.item = item
+
+
+class Trainer:
+    def __init__(self, pokemon, name=None):
+        self.pokemon = pokemon
+        self.name = name
+
+
 class GameState:
     def __init__(self):
         self.state = 'ongoing'
