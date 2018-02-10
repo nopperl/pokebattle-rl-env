1c7fd34609c2fada7920b7c8679432893ebfc859
nikuya3 <nikuya@dummy.mail>
2018-02-10 19:48:40 +0100

    Added useful showdown data and completed observation prototype

--- pokemon_battle_rl_env/poke_data_queries.py
+++ pokemon_battle_rl_env/poke_data_queries.py
@@ -1,8 +1,24 @@
 from json import load
 
-MOVES_PATH = 'data/moves.json'
+with open('data/abilities.json') as file:
+    abilities = load(file)['BattleAbilities']
+
+with open('data/items.json') as file:
+    items = load(file)['BattleItems']
+
 with open('data/moves.json') as file:
-    moves = load(file)
+    moves = load(file)['BattleMovedex']
+
+with open('data/pokedex.json') as file:
+    pokedex = load(file)['BattlePokedex']
+
+with open('data/typechart.json') as file:
+    typechart = load(file)['BattleTypeChart']
+
+genders = ['f', 'm', 'n']
+status_conditions = ['brn', 'par', 'slp', 'frz', 'psn', 'tox', 'confusion', 'trapped']
+targets = ['all', 'normal', 'self']
+weathers = ['raindance', 'primordialsea', 'sunnyday', 'desolateland', 'sandstorm', 'hail', 'deltastream']
 
 
 def move_id_to_name(id):
@@ -15,3 +31,7 @@ def move_name_to_id(name):
 
 def get_move_by_name(name):
     return next(m for m in moves.values() if m['name'] == name)
+
+
+def get_pokemon_by_species(species):
+    return next(p for p in pokedex.values() if p['species'] == species)
