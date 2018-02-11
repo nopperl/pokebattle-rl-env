01b039632c5cfa88f6a13cad4a5f0774602b5284
nikuya3 <nikuya@dummy.mail>
2018-02-11 14:55:09 +0100

    Completed first env prototype:

---
+++ test_agent.py
@@ -0,0 +1,17 @@
+import ray
+from ray.tune.registry import register_env, get_registry
+from ray.rllib import ppo
+from pokemon_battle_rl_env import BattleEnv
+
+env_creator_name = "PokeBattleEnv-v0"
+register_env(env_creator_name, lambda config: BattleEnv())
+
+ray.init()
+config = ppo.DEFAULT_CONFIG.copy()
+config['num_workers'] = 1
+agent = ppo.PPOAgent(config=config, env=env_creator_name, registry=get_registry())
+
+
+for i in range(10):
+    result = agent.train()
+    print("result: {}".format(result))
