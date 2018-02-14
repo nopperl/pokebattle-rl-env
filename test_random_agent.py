Fixed reward calculation and other minor bugs

test_random_agent.py
@@ -4,8 +4,10 @@ import numpy as np
 env = BattleEnv()
 for i in range(100):
     env.reset()
-    done = env.step(np.random.random(9))
+    _, reward, done, _ = env.step(np.random.random(9))
+    print(reward)
     while not done:
-        done = env.step(np.random.random(9))
+        _, reward, done, _ = env.step(np.random.random(9))
+        print(reward)
 env.close()
 print('Finished')