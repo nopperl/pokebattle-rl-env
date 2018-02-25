Fixed reward computation

random_agent.py
@@ -1,8 +1,9 @@
-from pokebattle_rl_env.pokebattle_env import PokeBattleEnv
-from pokebattle_rl_env.showdown_simulator import ShowdownSimulator
 import numpy as np
 
-env = PokeBattleEnv(ShowdownSimulator(local=True, self_play=True, debug_output=True))
+from pokebattle_rl_env.pokebattle_env import PokeBattleEnv
+from pokebattle_rl_env.showdown_simulator import ShowdownSimulator
+
+env = PokeBattleEnv(ShowdownSimulator(local=False, debug_output=True))
 for i in range(100):
     env.reset()
     _, reward, done, _ = env.step(np.random.random(11))