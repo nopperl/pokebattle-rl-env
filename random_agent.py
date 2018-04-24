82b39a5a77ed8660966d68c1bf810599afa918c7
nikuya3 <nikuya@dummy.mail>
2018-04-24 20:37:39 +0200

    Fixed random agent debug output

--- random_agent.py
+++ random_agent.py
@@ -3,7 +3,7 @@ import numpy as np
 from pokebattle_rl_env.pokebattle_env import PokeBattleEnv
 from pokebattle_rl_env.showdown_simulator import ShowdownSimulator, DEFAULT_PUBLIC_CONNECTION
 
-env = PokeBattleEnv(ShowdownSimulator(connection=DEFAULT_PUBLIC_CONNECTION, debug_output=True))
+env = PokeBattleEnv(ShowdownSimulator(connection=DEFAULT_PUBLIC_CONNECTION))
 for i in range(100):
     env.reset()
     _, reward, done, _ = env.step(np.random.random(11))
